4f18ddde285db201a7895da7f2783d4a1a679d60
Amal Jaison <amaljaison71@gmail.com>
2022-02-28 13:49:05 +0530

    [FEATURE] Showing quote details on quote detail page

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -2,14 +2,38 @@ import { Fragment } from "react";
 import { Route, useParams } from "react-router-dom";
 
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+  {
+    id: "q1",
+    author: "Carol Burnett",
+    text: "When you have a dream, you’ve got to grab it and never let go.",
+  },
+  {
+    id: "q2",
+    author: "Audrey Hepburn",
+    text: "Nothing is impossible. The word itself says ‘I’m possible!'",
+  },
+  {
+    id: "q3",
+    author: "Alexander the Great",
+    text: "There is nothing impossible to they who will try.",
+  },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
 
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found!</p>;
+  }
+
   return (
     <Fragment>
-      <h1>Quote Detail Page</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
