cff662e079b5ceea4f118c94d26207acfc195997
Amal Jaison <amaljaison71@gmail.com>
2022-02-28 13:21:01 +0530

    [FEATURE] Navigation links for different pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import { Redirect, Route, Switch } from "react-router-dom";
 
 import AllQuotes from "./components/pages/AllQuotes";
+import Layout from "./components/layout/Layout";
 import NewQuote from "./components/pages/NewQuote";
 import QuoteDetail from "./components/pages/QuoteDetail";
 
 function App() {
   return (
-    <Switch>
-      <Route path="/" exact>
-        <Redirect to="/quotes" />
-      </Route>
-      <Route path="/quotes" exact>
-        <AllQuotes />
-      </Route>
-      <Route path="/quotes/:quoteId">
-        <QuoteDetail />
-      </Route>
-      <Route path="/new-quote">
-        <NewQuote />
-      </Route>
-    </Switch>
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/quotes" />
+        </Route>
+        <Route path="/quotes" exact>
+          <AllQuotes />
+        </Route>
+        <Route path="/quotes/:quoteId">
+          <QuoteDetail />
+        </Route>
+        <Route path="/new-quote">
+          <NewQuote />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
